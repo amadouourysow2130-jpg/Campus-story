3bf04115c2f11278f4b0f8cad5ef375b8d58b7d0
Lysa Tran <thuyze@mbp-de-lysa.home>
2026-04-19 20:53:35 +0200

    avancé de la page css et de l'index

--- index.php
+++ index.php
@@ -1,11 +1,33 @@
+<?php require_once 'includes/session.php' ; ?>
 <!DOCTYPE html>
-<html lang="en">
+
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width= , initial-scale=1.0">
-    <title>Document</title>
+    <title>Campus Stories</title>
+    <link rel="stylesheet" href="css/style.css"> 
 </head>
+
 <body>
-    bonjour ! 
+    <nav class="navbar">
+        <h1>Campus Stories</h1>
+        <div class="menu">
+            <a href="index.php">Accueil</a>
+            <?php if(utilisateur_connecte()): ?>
+                <a href="create_story.php">Publier</a>
+                <a href="logout.php">Déconnexion</a>
+            <?php else: ?>
+                <a href="login.php">Connexion</a>
+            <?php endif; ?>
+        </div>
+    </nav>
+
+    <main id="feed_stories"> <!-- afficher les histoires -->
+        <p>Chargement des expériences et histoires...</p>
+    </main>
+
+    <script src="js/ajax.js"></script> <!-- fichier ajax.js relié mais peut être pas necessaire ? -->
+    <script src="js/main.js"></script> <!-- fichier java a relier plus tard -->
+
 </body>
 </html>
